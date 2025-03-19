6d11491eb2c9c481ebfb6d6f3f08045b60055a6c
Edward Ware <edwardware2006@gmail.com>
2025-03-19 09:44:56 -0500

    Added multilingual support

---
+++ helloworld.py
@@ -0,0 +1,29 @@
+class Character:
+    def __init__(self, char):
+        self.char = char
+
+    def get_char(self):
+        return self.char
+
+class Word:
+    def __init__(self, *chars):
+        self.chars = [Character(char) for char in chars]
+
+    def get_word(self):
+        return ''.join([char.get_char() for char in self.chars])
+
+class Sentence:
+    def __init__(self, *words):
+        self.words = [Word(*word) for word in words]
+
+    def get_sentence(self):
+        return ' '.join([word.get_word() for word in self.words])
+
+
+hello_word = Sentence(
+    ['h', 'e', 'l', 'l', 'o'],  
+    ['w', 'o', 'r', 'l', 'd']   
+)
+
+
+print(hello_word.get_sentence())
